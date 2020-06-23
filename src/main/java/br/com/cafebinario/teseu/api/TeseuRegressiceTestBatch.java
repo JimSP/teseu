ff2614f1c43f358195b3f1154e5756664c581f3a
Jane Pellim <janepellim@ext.cvccorp.com.br>
2020-06-23 01:59:05 -0300

    ajuste

--- src/main/java/br/com/cafebinario/teseu/api/TeseuRegressiceTestBatch.java
+++ src/main/java/br/com/cafebinario/teseu/api/TeseuRegressiceTestBatch.java
@@ -4,5 +4,5 @@ public interface TeseuRegressiceTestBatch {
 
 	ExecutionStatus execute(final Long testId);
 
-	ExecutionStatus execute(final String ordersName, final String teseuRunMode);
+	ExecutionStatus execute(final String ordersName, final String name);
 }
--- src/main/java/br/com/cafebinario/teseu/api/TeseuRegressiceTestRest.java
+++ src/main/java/br/com/cafebinario/teseu/api/TeseuRegressiceTestRest.java
@@ -1,10 +1,5 @@
 package br.com.cafebinario.teseu.api;
 
-import java.nio.file.Path;
-import java.util.List;
-
-import br.com.cafebinario.teseu.infrastruct.rest.dto.ExecutionApi;
-
 public interface TeseuRegressiceTestRest {
 
 	void execute(final Long testId);
